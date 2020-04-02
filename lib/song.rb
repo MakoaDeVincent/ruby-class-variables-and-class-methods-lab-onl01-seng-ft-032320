68fcb2b10c9bd226b2db97594d5b2a1252c98e24
Makoa DeVincent <makoadevincent@gmail.com>
2020-04-02 19:27:53 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -39,7 +39,7 @@ class Song
     genre_count = {}
     @@genres.each do |genre|
       if genre_count[genre]
-        genre_count += 1
+        genre_count ++
       else
         genre_count[genre] = 1
       end
