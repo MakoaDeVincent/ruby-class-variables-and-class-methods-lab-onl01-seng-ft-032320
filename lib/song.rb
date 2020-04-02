751aea8c16304cbda21d0c057c6c4243a4296929
Makoa DeVincent <makoadevincent@gmail.com>
2020-04-02 19:02:57 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,10 +2,15 @@ class Song
 
   attr_accessor :song_name, :artist, :genre
 
+  @@count = 0
+
   def initialize(song_name, artist, genre)
     @song_name = song_name
     @artist = artist
     @genre = genre
+
+    @@count += 1
+    
   end
 
   def name
